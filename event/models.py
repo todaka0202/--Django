322f7f76cc87d075695bf31400f62a127810ac3a
todaka0202 <yizaihugao@gmail.com>
2024-10-23 11:57:21 +0900

    adminにイベントを表示するところ

--- event/models.py
+++ event/models.py
@@ -1,8 +1,18 @@
-from django.db import models
+import datetime
 
-# class Event(models.Model):
-#     event_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField
+from django.db import models
+from django.utils import timezone
+
+
+class Event(models.Model):
+    event_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.event_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 # Create your models here.
