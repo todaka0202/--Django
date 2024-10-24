590b98c3e56cf9e1729b2801b0c61c31f1014d1d
todaka0202 <yizaihugao@gmail.com>
2024-10-24 12:20:29 +0900

    viewまで

--- event/views.py
+++ event/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Event
 
 
 class IndexView(TemplateView):
     template_name = "event/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Event.objects.order_by("-pub_date")[:5]
