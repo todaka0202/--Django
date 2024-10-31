9f745538f64ad37d19b93244d0ce741a0ee1d5c3
todaka0202 <yizaihugao@gmail.com>
2024-10-31 12:19:29 +0900

    formからdbにデータを送信するのができない；；

--- event/views.py
+++ event/views.py
@@ -3,19 +3,7 @@ from django.shortcuts import render
 from .models import Event
 
 
-# class IndexView(TemplateView):
-#     template_name = "event/index.html"
-#     context_object_name = "latest_question_list"
-
-#     def get_queryset(self):
-#         """Return the last five published questions."""
-#         return Event.objects.order_by("-pub_date")[:5]
-
-
 def index(request):
     latest_event_list = Event.objects.order_by("-pub_date")[:5]
     context = {"latest_event_list": latest_event_list}
     return render(request, "event/index.html", context)
-
-
-# def vote(request, event_id)
