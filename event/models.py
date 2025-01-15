a0746b335f3c9382019d7e1ef678742bece196c7
todaka0202 <yizaihugao@gmail.com>
2025-01-15 09:24:24 +0900

    完成

--- event/models.py
+++ event/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Event(models.Model):
-    event_text = models.CharField(max_length=200)
+    event_text = models.CharField(max_length=20)
 
     def __str__(self):
         return self.event_text
 
 
 class DateChoice(models.Model):
-    event_date = models.CharField(max_length=200)
+    event_date = models.CharField(max_length=20)
 
     def __str__(self):
         return self.event_date
